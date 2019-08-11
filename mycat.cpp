9eda7d5423a82a1343bdcb0a1d0490917b05b94c
Breno Guimaraes <brenorg@gmail.com>
2019-08-11 12:19:46 -0400

    mycat should cat all files passed as arguments

--- mycat.cpp
+++ mycat.cpp
@@ -4,11 +4,14 @@
 
 int main(int argc, char** argv)
 {
-    std::string symbol_data;
-    std::ifstream in(argv[1]);
-    std::getline(in, symbol_data, std::string::traits_type::to_char_type(
-                                  std::string::traits_type::eof()));
-    in.close();
-    std::cout << symbol_data;
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string symbol_data;
+        std::ifstream in(argv[i]);
+        std::getline(in, symbol_data, std::string::traits_type::to_char_type(
+                                      std::string::traits_type::eof()));
+        in.close();
+        std::cout << symbol_data;
+    }
     return 0;
 }
